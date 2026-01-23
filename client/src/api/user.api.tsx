e56dea2dae38abfc3efcbf9b0293c0948a3f54c2
mirza-shafi <mirza.md.shafi.uddin@gmail.com>
2026-01-24 00:41:18 +0600

    fix: update user profile API to use environment variable

--- client/src/api/user.api.tsx
+++ client/src/api/user.api.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { UserProfile, UpdateProfilePayload } from '../types/user.types';
 
-const API_URL = '/api/user/profile';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_URL = `${API_BASE}/api/user/profile`;
 
 export const getProfile = async (): Promise<UserProfile> => {
   const token = localStorage.getItem('userToken');
